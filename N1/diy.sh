b041ec9d0cc3cff300ef96f11a79ac7a45bfc3e5
xyself <133769218+xyself@users.noreply.github.com>
2024-09-01 06:10:35 +0800

    date

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
-
+# rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
 git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+#定时限速
+git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+#git clone https://github.com/lucikap/luci-app-nettask package/luci-app-nettask
+luci-app-banip
 # 插件
 echo "
+CONFIG_PACKAGE_luci-app-oaf=y
 CONFIG_PACKAGE_luci-app-mosdns=y
 CONFIG_PACKAGE_luci-app-mihomo=y
 CONFIG_PACKAGE_luci-app-tailscale=y
-CONFIG_PACKAGE_luci-luci-app-amlogic=y
+CONFIG_PACKAGE_luci-app-amlogic=y
+CONFIG_PACKAGE_luci-app-eqosplus=y
+CONFIG_PACKAGE_luci-app-pushbot=y
 " >> .config
+#CONFIG_PACKAGE_luci-app-nettask=y
 # Default IP
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
+
 
 # Add packages
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
