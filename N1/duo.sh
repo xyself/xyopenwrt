7c4908457863782997303da296757b22cb60c39f
xyself <133769218+xyself@users.noreply.github.com>
2024-09-30 04:38:36 +0800

    date

--- N1/duo.sh
+++ N1/duo.sh
@@ -1,4 +1,6 @@
+
 #!/bin/bash
+
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
@@ -10,14 +12,16 @@ function git_sparse_clone() {
 sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 # rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/v2ray-geodata
+rm -rf packages/net/adguardhome
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
 git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
-git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome amlogic luci-theme-alpha luci-theme-atmaterial_new luci-theme-ifit luci-app-fileassistant
+git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome amlogic luci-theme-alpha luci-app-fileassistant
 #git clone https://github.com/linkease/istore package/istore/luci-app-store
 #git_sparse_clone master https://github.com/linkease/istore luci/luci-app-store
 #定时限速
+#luci-theme-atmaterial_new luci-theme-ifit
 #git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
 #git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
