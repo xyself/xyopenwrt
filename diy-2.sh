07b641b9750b42bfeb66df02fb81e63ca80049fc
xyself <133769218+xyself@users.noreply.github.com>
2026-04-29 23:57:57 +0800

    Update diy-2.sh
    Signed-off-by: xyself <133769218+xyself@users.noreply.github.com>

--- diy-2.sh
+++ diy-2.sh
@@ -2,3 +2,8 @@
 
 # 添加包
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+# 修改 luci-app-tailscale-community
+sed -i 's/LUCI_DEPENDS:=+tailscale +ip +luci-base/LUCI_DEPENDS:=+tailscale +ip-full +luci-base/g' feeds/luci/applications/luci-app-tailscale-community/Makefile
+
+# 修改 zerotier
+sed -i 's/+ip +libminiupnpc/+ip-full +libminiupnpc/g' feeds/packages/net/zerotier/Makefile
